e6f6e9c7d663bb474ee9c4d38e1f0710c40e67e2
Russell Brown <russellbrown798@gmail.com>
2026-03-25 19:53:51 -0400

    added temp variable to fix issue with in-place processing

--- synth-plugin/dsp/src/DipoleRadiation.cpp
+++ synth-plugin/dsp/src/DipoleRadiation.cpp
@@ -7,10 +7,13 @@ void DipoleRadiation::processBlock(float *in, float *out, unsigned int length)
 {
     for (unsigned int i{ 0 }; i < length; i++)
     {
-        out[i] = in[i] - 3.f * m_x1 + 3.f * m_x2 - m_x3;
+        float x = in[i];
+
+        out[i] = x - 3.f * m_x1 + 3.f * m_x2 - m_x3;
+
         m_x3 = m_x2;
         m_x2 = m_x1;
-        m_x1 = in[i];
+        m_x1 = x;
     }
 }
-};
+}
