d015ce6730a49cfeb1f7eaee66e76984cc729afb
Russell Brown <russellbrown798@gmail.com>
2026-03-26 16:43:16 -0400

    removed unused constants

--- synth-plugin/dsp/include/ModalBank.h
+++ synth-plugin/dsp/include/ModalBank.h
@@ -3,21 +3,27 @@
 namespace ModalSynth
 {
 const int MODE_SHAPE_LUT_SIZE{ 64 };
-const float MAX_R{ 0.99999f };
-const float MAX_R_SQUARED{ powf(MAX_R, 2.0f) };
-
 const int NUM_MODES{ 8 };
+
+const float MIN_BAR_GLOBAL_DAMPING{ 0.5f };
+const float MAX_BAR_GLOBAL_DAMPING{ 10.f };
+const float MIN_BAR_OVERTONE_DAMPING{ 0.006f };
+const float MAX_BAR_OVERTONE_DAMPING{ 0.2f };
+const float BAR_NOTE_DAMPING{ 1.5e-6f };
 
 class ModalBank
 {
 private:
     float m_sampleRate{};
     float m_freq[NUM_MODES]{};
-    float m_radius[NUM_MODES]{};
     float m_gain[NUM_MODES]{};
     float m_positionGain[NUM_MODES]{};
     float m_outPosGain[NUM_MODES]{};
     float m_overtoneRatios[NUM_MODES-1]{};
+    float m_alpha[NUM_MODES]{};
+
+    float m_globalDamping{};
+    float m_overtoneDamping{ 0.026f };
 
     float m_f1{};
 
