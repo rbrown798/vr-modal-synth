6561feb14e89e2694450c86b6a14a18c3d58a088
Russell Brown <russellbrown798@gmail.com>
2026-03-27 15:30:43 -0400

    added bar metallic slider

--- vr-modal-synth-app/Assets/ModalVR/Scripts/SetDamping.cs
+++ vr-modal-synth-app/Assets/ModalVR/Scripts/SetDamping.cs
@@ -6,12 +6,10 @@ using UnityEngine.Audio;
 
 public class SetDamping : MonoBehaviour
 {
-	public Material barMaterial;
 	public AudioMixer mixer;
     
     public void SetDampingFromValue(float value)
     {
-		barMaterial.SetFloat("_Smoothness", 0.5f + (1f - value) * 0.5f);
 		mixer.SetFloat("BarDamping", value);
     }
 }
---
+++ vr-modal-synth-app/Assets/ModalVR/Scripts/SetMetallic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetMetallic : MonoBehaviour
+{
+	public Material barMaterial;
+	public AudioMixer mixer;
+
+	public void SetMetallicFromValue(float value)
+	{
+		barMaterial.SetFloat("_Smoothness", 0.5f + value * 0.5f);
+		mixer.SetFloat("BarMetallic", value);
+	}
+}
