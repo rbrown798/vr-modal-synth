5d2e1945a9118d17126fd11a2b40485cd2c615cb
Russell Brown <russellbrown798@gmail.com>
2026-04-02 09:17:42 -0400

    added Parameters struct

---
+++ synth-plugin/dsp/include/Parameters.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace ModalSynth
+{
+struct Parameters
+{
+    float barTimbre;
+    float barDamping;
+    float barMetallic;
+    float malletSize;
+    float malletStiffness;
+    bool tubeOn;
+    bool tremoloOn;
+    float tremoloRate;
+};
+};
